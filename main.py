9595a766846fc59c7557085e4bc61e784caea9f3
yyf <1095278981@qq.com>
2023-02-26 17:50:21 +0800

    bj_dataset

---
+++ main.py
@@ -0,0 +1,148 @@
+import json
+from datetime import datetime
+
+import pandas as pd
+
+def geometry2json(geometry):
+    json = geometry.replace('POLYGON ','')
+    json = json.replace('((','[[*[')
+    json = json.replace('))',']*]]')
+    json = json.replace(', ','],*[')
+    json = json.replace(' ',',')
+    json = json.replace('*',' ')
+    return json
+
+
+
+geo_file = pd.DataFrame(columns=['geo_id','type','coordinates','function','traffic_type'])
+#.geo
+#region
+region_bj = pd.read_csv('Data/region_bj.csv',delimiter=',')
+region_num = len(region_bj)
+index = 0
+for i in range(len(region_bj)):
+    type = 'Polygon'
+    coordinates = geometry2json(region_bj.loc[i,'geometry'])
+    function = region_bj.loc[i,'FUNCTION']
+    traffic_type = 'region'
+    geo_file.loc[index] = [index,type,coordinates,function,traffic_type]
+    index += 1
+#road
+road_bj = pd.read_csv('Data/road_bj.csv',delimiter=',')
+road_num = len(road_bj)
+for i in range(len(road_bj)):
+    type = 'LineString'
+    coordinates = road_bj.loc[i,'coordinates']
+    function = -1
+    traffic_type = 'road'
+    geo_file.loc[index] = [index,type,coordinates,function,traffic_type]
+    index += 1
+#poi
+poi_bj = pd.read_csv('Data/POI_bj.csv',delimiter=',')
+poi_num = len(poi_bj)
+for i in range(len(poi_bj)):
+    type = 'Point'
+    x = poi_bj.loc[i,'X']
+    y = poi_bj.loc[i,'Y']
+    coordinates = '['+str(x)+','+str(y)+']'
+    function = poi_bj.loc[i,'P']
+    traffic_type = 'poi'
+    geo_file.loc[index] = [index,type,coordinates,function,traffic_type]
+    index += 1
+#save .geo
+geo_file.to_csv('result/bj_dataset.geo',index = False)
+
+#usr---没有额外的信息不需要单独一张表了
+#Rel
+rel_file = pd.DataFrame(columns=['rel_id','type','origin_id','destination_id','rel_type'])
+index = 0
+road2region = pd.read_json('Data/road2region_bj.json', typ='series')
+for i in range(len(road2region)):
+    type = 'geo'
+    origin_id = i+region_num
+    destination_id = road2region[i]
+    rel_type = 'road2region'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+
+region2road = pd.read_json('Data/region2road_bj.json', typ='series')
+for i in range(len(region2road)):
+    type = 'geo'
+    origin_id = i
+    destination_id = region2road[i]+region_num
+    rel_type = 'region2road'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+
+poi2region = pd.read_json('Data/poi2region_bj.json', typ='series')
+for i in range(len(poi2region)):
+    type = 'geo'
+    origin_id = i+region_num+road_num
+    destination_id = poi2region[i]
+    rel_type = 'poi2region'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+
+region2poi = pd.read_json('Data/region2poi_bj.json', typ='series')
+for i in range(len(region2poi)):
+    type = 'geo'
+    origin_id = i
+    destination_id = region2poi[i]+region_num+road_num
+    rel_type = 'region2poi'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+
+poi2road = pd.read_json('Data/poi2road_bj.json', typ='series')
+for i in range(len(poi2road)):
+    type = 'geo'
+    origin_id = i+region_num+road_num
+    destination_id = poi2road[i]+region_num
+    rel_type = 'poi2road'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+
+road2poi = pd.read_json('Data/road2poi_bj.json', typ='series')
+for i in range(len(road2poi)):
+    type = 'geo'
+    origin_id = i+region_num
+    destination_id = road2poi[i]+region_num+road_num
+    rel_type = 'road2poi'
+    rel_file.loc[index] = [index,type,origin_id,destination_id,rel_type]
+    index+=1
+rel_file.to_csv('result/bj_dataset.rel',index = False)
+
+#dyna
+dyna_file = pd.DataFrame(columns=['dyna_id','type','time','entity_id','traj_id','geo_id'])
+traj = pd.read_csv('Data/traj_bj.csv',delimiter=';')
+index = 0
+for i in range(len(traj)):
+    path = traj.loc[i,'path']
+    path = path[1:len(path)-1].split(',')
+    path = [int(s) for s in path]
+    t_list = traj.loc[i,'tlist']
+    t_list = t_list[1:len(t_list)-1].split(',')
+    t_list = [int(s) for s in t_list]
+    t_list = [datetime.datetime.utcfromtimestamp(tim) for tim in t_list]
+    type = 'trajectory'
+    entity_id = traj.loc[i,'usr_id']
+    traj_id = traj.loc[i,'traj_id']
+    for j in range(len(path)):
+        time = t_list[j]
+        geo_id = path[j]
+        dyna_file.loc[index] = [index,type,time,entity_id,traj_id,geo_id]
+        index+=1
+dyna_file.to_csv('result/bj_dataset.dyna',index=False)
+
+#config
+geo_obj = {"including_types":["Point", "LineString", "Polygon"],"Point":{"function":"other","traffic_type":"enum"},"LineString":{"function":"other","traffic_type":"enum"},"Polygon":{"function":"num","traffic_type":"enum"}}
+rel_obj = {"including_types":["geo"],"geo":{"rel_type":"enum"}}
+dyna_obj = {"including_types":["trajectory"],"trajectory":{"geo_id":"geo_id"}}
+info_obj = {"data_files":"bj_dataset","geo_file":"bj_dataset","rel_file":"bj_dataset","calculate_weight_adj":True}
+config_obj = {"geo":geo_obj,"rel":rel_obj,"dyna":dyna_obj,"info":info_obj}
+with open('result/config.json','w') as f:
+    json.dump(config_obj,f,indent = 2)
+
+
+
+
+
